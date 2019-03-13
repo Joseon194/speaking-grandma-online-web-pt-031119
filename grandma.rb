b04e48e4a5b4578ae4d02d44f69ee9f6698fd1e6
Dennis Chung <chungdennis6@gmail.com>
2019-03-13 20:39:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma
+def speak_to_grandma(string)
   if (string == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
   elsif (string == string.upcase)
